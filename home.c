2d1fca467aea1b50b9dcf716c8f42fb58e0daa95
COLDRAMEN1221 <mhillyer27@germantownfriends.org>
2025-08-20 12:30:43 -0400

    modified:   home.c

--- home.c
+++ home.c
@@ -9,13 +9,13 @@
 int main() {
     system(CLEAR_CMD);
     int input = 0;
-    printf("1 - snake game\n");
+    printf("1 - snake game\n"); 
     printf("2 - dinosaur game\n");
     printf("> ");
     scanf("%d", &input);
     switch(input) {
-        case 1: system("make snake_game; ./snake_game");
-        case 2: system("make Dino; ./Dino");
+        case 1: system("make snake_game; ./snake_game"); break;
+        case 2: system("make Dino; ./Dino"); break;
     }
     return 0;
 }
